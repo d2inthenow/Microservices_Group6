cb2c942891d47d49e34db02e6acd6fe4c684c49c
duong\DUONG <duomg0023@gmail.com>
2025-04-09 18:16:07 +0700

    Routers & middleware (error handler)

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signoutRouter } from "./routes/signout";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hello from auth service!!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// Middleware to handle errors
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Auth service is running on port 3000!");
