341566e33d32a95a0894765e15e28a8d3c70fc03
duong\DUONG <duomg0023@gmail.com>
2025-04-14 14:51:42 +0700

    create user && hash password

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors"; // Giúp xử lý các lỗi bất đồng bộ
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -25,6 +26,17 @@ app.all("*", async (req, res, next) => {
 // Middleware xử lý lỗi
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Auth service listening on port 3000!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Auth service listening on port 3000!");
+  });
+};
+
+start();
