791f409f2bcc643a94e0f32f7bfb5fd5444a49b3
duong\DUONG <duomg0023@gmail.com>
2025-04-17 09:11:33 +0700

    chia current-user thanh 1 middleware and add require-auth

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,20 +1,13 @@
 import express = require("express");
 import jwt from "jsonwebtoken";
 
+import { requireAuth } from "../middlewares/require-auth";
+import { currentUser } from "../middlewares/current-user";
+
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  if (!req.session?.jwt) {
-    res.send({ currentUser: null });
-    return;
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", currentUser, requireAuth, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
