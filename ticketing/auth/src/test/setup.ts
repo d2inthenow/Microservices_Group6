000e6bc100e60048dbcb66d2245bb239e9999f6d
duong\DUONG <duomg0023@gmail.com>
2025-04-17 18:45:58 +0700

    create helper function

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -30,3 +35,21 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "duong@gmail.com";
+  const password = "12345";
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+  const cookie = response.get("Set-Cookie");
+
+  if (!cookie) {
+    throw new Error("No cookie returned from signup");
+  }
+  return cookie;
+};
