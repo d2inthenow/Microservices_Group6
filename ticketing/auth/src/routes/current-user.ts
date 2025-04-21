9ff98418bff18c927e0c3d197289d385191eec3e
duong\DUONG <duomg0023@gmail.com>
2025-04-22 00:23:48 +0700

    tach error va middleware o service auth qua package common de su dung chung

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,7 +1,6 @@
 import express = require("express");
-import jwt from "jsonwebtoken";
-import { requireAuth } from "../middlewares/require-auth";
-import { currentUser } from "../middlewares/current-user";
+
+import { currentUser } from "@d2tickets/common";
 
 const router = express.Router();
 
