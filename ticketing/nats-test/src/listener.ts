d8a285006668e30d73ffe92d1496d9f234f01379
duong\DUONG <duomg0023@gmail.com>
2025-04-30 00:13:48 +0700

    refactor file listener

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -15,29 +16,8 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("accounting-service");
-
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "queue-group-name",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(
-        `Message received: ${msg.getSequence()} , with data: ${data}`
-      );
-    }
-    msg.ack(); // Acknowledge the message
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
-process.on("SIGINT", () => stan.close()); // Close the connection when the process is interrupted (Ctrl+C)
-process.on("SIGTERM", () => stan.close()); // Close the connection when the process is terminated (e.g., in a Kubernetes pod shutdown)
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
