b039fee54fb90425cdd3e1f94b3be8eef64b96a3
duong\DUONG <duomg0023@gmail.com>
2025-05-02 14:23:52 +0700

    use enum subject and Enforcing Listener subjects , Data Types

--- ticketing/nats-test/src/events/ticket-created-listener.ts
+++ ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -1,14 +1,15 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payments-service";
-
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event data!", data);
 
-    // Acknowledge the message
+    // Acknowledge the messages
     msg.ack();
   }
 }
