287afa304409ba0e3fb056e355727f8c43a43cb5
duong\DUONG <duomg0023@gmail.com>
2025-05-10 16:37:53 +0700

    Khởi tạo và Chia sẻ Client NATS , Triển khai Singleton NATS Wrapper , Triển khai và Kiểm tra Graceful Shutdown

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
-import { requireAuth, validateRequest } from "@d2tickets/common";
 import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@d2tickets/common";
 import { Ticket } from "../models/tickets";
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -18,15 +19,13 @@ router.post(
   validateRequest,
   async (req: Request, res: Response) => {
     const { title, price } = req.body;
-
     const ticket = Ticket.build({
       title,
       price,
       userId: req.currentUser!.id,
     });
     await ticket.save();
-
-    new TicketCreatedPublisher(client).publish({
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
